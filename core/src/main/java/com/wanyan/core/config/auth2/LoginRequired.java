45983181fc01fb43e97b9300af801fd209eee082
wanyanhw <2516452572@qq.com>
2021-04-30 20:27:28 +0800

    jwt配置：TODO

---
+++ core/src/main/java/com/wanyan/core/config/auth2/LoginRequired.java
@@ -0,0 +1,12 @@
+package com.wanyan.core.config.auth2;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginRequired {
+    boolean required() default true;
+}
