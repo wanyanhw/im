0c99aaea550efb8ca5342548f0de96893deb1f45
wanyanhw <wyhwlml@126.com>
2022-05-24 20:03:56 +0800

    随机生成客户端名称

--- im-client/src/main/java/com/wanyan/imclient/ImClientApplication.java
+++ im-client/src/main/java/com/wanyan/imclient/ImClientApplication.java
@@ -11,7 +11,13 @@ public class ImClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ImClientApplication.class, args);
-        CacheUtil.setClientName(args[0]);
+        String clientName;
+        if (args == null || args.length == 0) {
+            clientName = "Robot" + (int) (Math.random() * 100);
+        } else {
+            clientName = args[0];
+        }
+        CacheUtil.setClientName(clientName);
         new Thread(() -> new ImClient().run("192.168.100.10", 8888)).start();
     }
 
